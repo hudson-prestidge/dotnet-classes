ad49438514ce15f97c778896c32d20209fdf6b3d
Hudson Prestidge <charcon@ihug.co.nz>
2016-10-11 17:34:38 +1300

    class method working, second instance of class created

--- Program.cs
+++ Program.cs
@@ -13,7 +13,16 @@ namespace ConsoleApplication
             myCar.Color = "Purple";
 
             Console.WriteLine("{0} {1} {2} {3}",myCar.Make, myCar.Model, myCar.Year, myCar.Color);
+            decimal value = myCar.DetermineMarketValue();
+            Console.WriteLine("car value is: {0:C}", value);
+
+            Car myOtherCar;
+            myOtherCar = myCar;
+            Console.WriteLine("{0} {1} {2} {3}",myOtherCar.Make, myOtherCar.Model, myOtherCar.Year, myOtherCar.Color);
+           
         }
+
+
     }
 
     public class Car
@@ -22,5 +31,12 @@ namespace ConsoleApplication
         public string Model { get; set; }
         public int Year { get; set; }
         public string Color { get; set; }
+        public decimal DetermineMarketValue ()
+      {
+            decimal carValue = 100.0M;
+
+            return carValue;
+     }
+   
     }
 }
