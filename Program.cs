7aa926b76d51ed9b6af7e92fddf4f7fc32a01d5c
Hudson Prestidge <charcon@ihug.co.nz>
2016-10-11 16:57:09 +1300

    class and methods working

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car myCar = new Car();
+            myCar.Make = "Tesla";
+            myCar.Model = "Guybrush Threepwood";
+            myCar.Year = 1200;
+            myCar.Color = "Purple";
+
+            Console.WriteLine("{0} {1} {2} {3}",myCar.Make, myCar.Model, myCar.Year, myCar.Color);
         }
+    }
+
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
     }
 }
